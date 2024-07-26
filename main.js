7540a64dde53480ca67382107bd7711ceb793ef6
AnaAmora <104854292+AnaAmora@users.noreply.github.com>
2024-07-26 13:58:50 +0000

    .

---
+++ main.js
@@ -0,0 +1,47 @@
+const caixaPrincipal= document.queryselector(".caixa-principal");
+const caixaPerguntas= document.queryselector(".caixa-perguntas");
+const caixaAlternativas= document.queryselector(".caixa-alternativas");
+const caixaResultado= document.queryselector(".caixa-resultado");
+const textoResultado= document.queryselector(".texto-resultado");
+
+
+
+
+
+const perguntas =[
+    {
+      enunciado:"Você gosta bolo?",
+      alternativas:[
+        {
+            texto:"sim",
+            afirmacao:"Você é uma pessoa normal"
+        },
+            
+        {
+            texto:"não",
+            afirmacao:"Você é chato pra carambolas"
+        }
+    ]
+    },
+    {
+        enunciado:"Você dorme?",
+        alternativas:[
+          {
+              texto:"sim",
+              afirmacao:"Você é uma pessoa normal"
+          },
+              
+          {
+              texto:"não",
+              afirmacao:"Você é muito estranho"
+          }
+      ]
+    }
+
+]
+
+
+function mostraPergunta(){
+
+}
+    
